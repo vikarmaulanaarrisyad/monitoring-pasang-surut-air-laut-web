d6c98b331cdcd16c848d61687aa284fe6623bf64
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2023-06-09 21:13:09 +0700

    update

--- resources/views/layouts/partials/front_footer.blade.php
+++ resources/views/layouts/partials/front_footer.blade.php
@@ -1,7 +1,7 @@
   <footer id="footer">
     <div class="footer-top">
       <div class="container">
-        <div class="row">
+        {{-- <div class="row">
 
           <div class="col-lg-4 col-md-6">
             <div class="footer-info">
@@ -34,16 +34,7 @@
             </ul>
           </div>
 
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
+
 
           <div class="col-lg-4 col-md-6 footer-newsletter">
             <h4>Our Newsletter</h4>
@@ -54,20 +45,15 @@
 
           </div>
 
-        </div>
+        </div> --}}
+           <div class="copyright">
+        &copy; Copyright <strong><span>Bootslander</span></strong>. All Rights Reserved
+      </div>
       </div>
     </div>
 
     <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Bootslander</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+
+
     </div>
   </footer>
